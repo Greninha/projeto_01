f7403160830ff47b0259a714e2de6a4dcb22f6ab
João Marcos <joaomarcos.bss@gmail.com>
2020-02-07 10:15:49 -0300

    Meu primeiro comit

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+	<head>
+		<meta charset="utf-8">
+		<title>Laboratorio Web Aula 01</title>
+	</head>
+	<body>
+		<?php 
+
+			echo "<p>Olá, bom dia.<br>Aula Programação Web</p>";
+			echo "<br><br><br><br><br><strong><p>Aluno:João Marcos Francelino Macêdo<br><br>Nº 20</p></strong>";
+
+		 ?>	
+	</body>
+</html>
